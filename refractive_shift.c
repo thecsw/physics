911a499efd4da6f1ccb0911dd25d55eee46cfa6a
thecsw <thecsmoothiew@yahoo.com>
2017-12-12 17:50:36 +0400

    Refractive shift code

---
+++ refractive_shift.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+#define PI 3.14159265
+
+void var_input(char* str, float* f) {
+  printf(str);
+  scanf("%f", f);
+}
+
+int main(int argc, char** argv) {
+  float a, d, n1, n2, x;
+  var_input("Incident angle : ", &a);
+  var_input("Glass thicc    : ", &d);
+  var_input("Index 1        : ", &n1);
+  var_input("Index 2        : ", &n2);
+  x = ((d*(cos(a*(PI/180)+asin((n1*cos(a*(PI/180)))/(n2)))))/(cos(asin((n1*cos(a*(PI/180)))/(n2)))));
+  printf("Refractive shift : %f\n", x);
+  return 0;
+}
