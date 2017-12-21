7b3c3e5bb38759218d8b4f9a6e52ad0ffc531289
thecsw <thecsmoothiew@yahoo.com>
2017-12-21 15:12:40 +0400

    Commented the things needed for loops and animations

--- Weierstrass.py
+++ Weierstrass.py
@@ -7,30 +7,29 @@ b = 0.1
 lim = 200
 w = []
 p = []
-i=-2
+i = -2
 o = 0
 begin = time.time()
-while b < 10:
-    i=-2
-    while i < 2:
-        x = 0
-        for n in range(0, lim):
-            bn = math.pow(b, n)
-            cosx =  math.cos(bn * math.pi * i)
-            x += math.pow(a, n) * cosx
-        w.append(x)
-        p.append(i)
-        print(i)
-        i+=0.001
-    b+=0.1
-    o+=1
-    plt.title("b = {}".format(b))
-    plt.plot(p, w, linewidth=0.5)
-    p = []
-    w = []
-    plt.savefig('Weierstrass-Color/{}.png'.format(o), dpi=300, format='png')
-    #plt.clf()
+#while b < 10:
+#    i = -2
+while i < 2:
+    x = 0
+    for n in range(0, lim):
+        bn = math.pow(b, n)
+        cosx =  math.cos(bn * math.pi * i)
+        x += math.pow(a, n) * cosx
+    w.append(x)
+    p.append(i)
+    print(i)
+    i+=0.001
+#   b+=0.1
+#   o+=1
+plt.title("b = {}".format(b))
+plt.plot(p, w, linewidth=0.5)
+#    p = []
+#    w = []
+#    plt.savefig('Weierstrass-Color/{}.png'.format(o), dpi=300, format='png')
+#plt.clf()
 end = time.time()
 print("Execution time : {} seconds".format(end-begin))
-
 plt.show()
