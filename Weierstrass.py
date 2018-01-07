6d8011230103dc2bfcef56dfa621b2e248f35b79
thecsw <thecsmoothiew@yahoo.com>
2018-01-07 19:37:20 +0400

    Plotting

--- Weierstrass.py
+++ Weierstrass.py
@@ -2,27 +2,22 @@ import math
 import matplotlib.pyplot as plt
 import time
 # Comments are the version for animations.
-#demo = input("Would you like the demo version with default values? (Y/n) ")
-#if demo == 'Y':
-#    a = 0.2
-#    b = 4.5
- #   iteration_step = 0.001
- #   min_x = -2
- #   max_x = 2
-#else:
-#    a = input("Please input value for a : ")
-#    b = input("Please input value for b : ")
-#    iteration_step = input("Please input the iteration value, less is more accurate (Default = 0.001) : ")
-#    min_x = input("Lower boundary for x : ")
-#    max_x = input("Upper boundary for x : ")
-a = 0.2
-b = 4.5
-iteration_step = 0.001
-min_x = -2
-max_x = 2
+demo = input("Would you like the demo version with default values? (Y/n) ")
+if demo == 'Y':
+    a = 0.2
+    b = 4.5
+    iteration_step = 0.001
+    min_x = -2
+    max_x = 2
+else:
+    a = input("Please input value for a : ")
+    b = input("Please input value for b : ")
+    iteration_step = input("Please input the iteration value, less is more accurate (Default = 0.001) : ")
+    min_x = input("Lower boundary for x : ")
+    max_x = input("Upper boundary for x : ")
 y_values = []
 x_values = []
-iteration_limit = 200
+iteration_limit = 50
 begin = time.time()
 x = min_x
 save = open('Example', 'w')
@@ -35,9 +30,8 @@ while x < max_x:
     x_values.append(x)
     save.write("{},{}\n".format(f, x))
     x += iteration_step
-#    print(x)
-#plt.title("Weierstrass function with parameteres : a = {}, b = {}".format(a, b))
-#plt.plot(x_values, y_values, linewidth = 0.5)
+plt.title("Weierstrass function with parameteres : a = {}, b = {}".format(a, b))
+plt.plot(x_values, y_values, linewidth = 0.5)
 save.close()
 end = time.time()
 print("Execution time : {} seconds".format(end-begin))
